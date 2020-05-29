0c760b3e29419be4277094dc7d2a73cd63c94bac
Adam Bennett <nyoxidetwitter@gmail.com>
2020-05-29 19:25:19 -0400

    init

---
+++ StartingGoldMod/GoldMod.cs
@@ -0,0 +1,41 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+
+namespace StartingGoldMod
+{
+    [BepInPlugin("nyoxide.monstertrain.testmod", "Test Mod", "1.0.0.1")]
+    [BepInProcess("MonsterTrain.exe")]
+    [BepInProcess("MtLinkHandler.exe")]
+    public class GoldMod : BaseUnityPlugin
+    {
+        private static ConfigEntry<int> _goldAmt;
+        private static ConfigEntry<bool> _useCustomAmt;
+        
+        [HarmonyPatch(typeof(SaveManager), "GetStartingGoldForClass")]
+        static class GoldPatch 
+        {
+            static void Postfix(ref int __result)
+            {
+                __result = _goldAmt.Value;
+            }
+        }
+
+        void Patch()
+        {
+            var harmony = new Harmony("nyoxide.monstertrain.testmod.harmony");
+            harmony.PatchAll();
+        }
+
+        void Awake()
+        {
+            _useCustomAmt = Config.Bind("General", "Customize Gold Amount", true, "If you set this to false, vanilla Monster Train settings will be used to set your starting gold each run.");
+            _goldAmt = Config.Bind("General", "Gold Amount", 420, "The amount of gold to start each run with.");
+            if (_useCustomAmt.Value)
+            {
+                Patch();
+            }
+        }
+
+    }
+}
