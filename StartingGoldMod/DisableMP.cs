9982eb6980fb99f102d666ec4f792f231411e9b0
Adam Bennett <nyoxidetwitter@gmail.com>
2020-05-30 23:59:07 -0400

    changed mod name

--- StartingGoldMod/DisableMP.cs
+++ StartingGoldMod/DisableMP.cs
@@ -4,7 +4,7 @@ using ShinyShoe;
 
 namespace StartingGoldMod
 {
-    // Credit to Rawsome, Stable Infery for this: a quick and dirty patch to disable the multiplayer button.
+    // Credit to Rawsome, Stable Infery for this: a quick and dirty patch to disable the multiplayer button
     [HarmonyPatch(typeof(MainMenuScreen), "CollectMenuButtons")]
     static class MainMenuScreen_CollectMenuButtons_Patch
     {
--- StartingGoldMod/GoldMod.cs
+++ StartingGoldMod/GoldMod.cs
@@ -4,7 +4,7 @@ using HarmonyLib;
 
 namespace StartingGoldMod
 {
-    [BepInPlugin("nyoxide.monstertrain.starting-gold-mod", "Starting Gold Mod", "1.0.0.2")]
+    [BepInPlugin("nyoxide.monstertrain.starting-gold-mod", "Moneybags", "1.0.0.3")]
     [BepInProcess("MonsterTrain.exe")]
     [BepInProcess("MtLinkHandler.exe")]
     public class GoldMod : BaseUnityPlugin
@@ -14,13 +14,13 @@ namespace StartingGoldMod
         
         void Patch()
         {
-            var harmony = new Harmony("nyoxide.monstertrain.testmod.harmony");
+            var harmony = new Harmony("nyoxide.monstertrain.harmony");
             harmony.PatchAll();
         }
 
         void Awake()
         {
-            _useCustomAmt = Config.Bind("General", "Customize Gold Amount", true, "If you set this to false, vanilla Monster Train settings will be used to set your starting gold each run.");
+            _useCustomAmt = Config.Bind("General", "Enable Moneybags", true, "If you set this to false, vanilla Monster Train settings will be used to set your starting gold each run.");
             _goldAmt = Config.Bind("General", "Gold Amount", 420, "The amount of gold to start each run with.");
             if (_useCustomAmt.Value) 
                 Patch();
